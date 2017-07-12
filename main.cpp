3d52dbce05a800db69ba5bc0bdab4e6f87b93847
maksimpiriyev <maksimpiriyev@gmail.com>
2017-07-12 22:22:33 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,89 @@
+//
+//  main.cpp
+//  SerialPortRepeater
+//
+//  Created by Maksim Piriyev on 7/7/16.
+//  Copyright © 2016 Maksim. All rights reserved.
+//
+
+#include <iostream>
+#include <thread>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "serial_port.h"
+#define BUFFER_LENGTH 1000
+
+
+
+static SerialPort* serial1,*serial2;
+static int pipe_fd = -1;
+static int pipe_dronee = -1;
+// Define the function to be called when ctrl-c (SIGINT) signal is sent to process
+void
+signal_callback_handler(int signum)
+{
+    serial1->close_serial();
+    serial2->close_serial();
+    close(pipe_fd);
+    exit(signum);
+}
+
+int main(int argc, const char * argv[]) {
+    if (argc >= 6) {
+  
+    int baudRate1 = 115200,baudRate2=115200;
+    const char* port1,*port2;
+    //const char* pipe = NULL;
+    
+    printf("%d\n",argc);
+    if(argc==3){
+        port1 = argv[1];
+        port2 = argv[2];
+    }else if(argc>=5){
+        port1 = argv[1];
+        port2 = argv[3];
+        sscanf(argv[2], "%d",&baudRate1);
+        sscanf(argv[4], "%d",&baudRate2);
+    }
+   
+try{
+    static SerialPort sp1(port1,baudRate1),sp2(port2,baudRate2);
+    sp1.open_serial();
+    sp2.open_serial();
+    serial1 = &sp1; serial2 = &sp2;
+    
+    signal(SIGINT, signal_callback_handler);
+
+    
+    static char buffer1[BUFFER_LENGTH],buffer2[BUFFER_LENGTH];
+    const int L = 0;
+    thread([](){
+        while(true){
+            int l = sp2._read_port(buffer2,BUFFER_LENGTH);
+            if(l>0){
+                sp1._write_port(buffer2, l);
+                
+            }
+            usleep(5*1000);
+        }
+      
+    }).detach();
+ 
+    
+    while(true){
+        int l = sp1._read_port(buffer1,BUFFER_LENGTH);
+        if(l>0){
+            sp2._write_port(buffer1, l);
+        }
+        usleep(5*1000);
+        
+    }
+ 
+    
+}catch(...){ }
+    
+    return 0;
+}
+
